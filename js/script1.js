561482004bfeca789b58c1540eabc41079236e40
Bogdan <ki4ack.bogdan@gmail.com>
2023-05-07 23:09:35 +0300

    com

---
+++ js/script1.js
@@ -0,0 +1,19 @@
+// const shouldRenew = confirm('Хотите продлить подписку?');
+// console.log(shouldRenew);
+
+// let quantity = prompt('Kol-vo');
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
+
+// Integer Parser
+
+// let elementWidth = '50.22px';
+// console.log(typeof elementWidth);
+
+// elementWidth = Number.parseInt(elementWidth);
+// console.log(elementWidth);
+
+// console.log('Result:', typeof elementWidth);
+// console.log(typeof elementWidth);
+
