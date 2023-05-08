7f6975638ec62ac26fcb3dc19aebc4e01407d9be
Bogdan <ki4ack.bogdan@gmail.com>
2023-05-08 08:11:05 +0300

    goodmorning

--- js/script1.js
+++ js/script1.js
@@ -17,3 +17,35 @@
 // console.log('Result:', typeof elementWidth);
 // console.log(typeof elementWidth);
 
+// toFixed сокращение числа после точки
+
+// let salary = 1005.13232;
+// salary = Number(salary.toFixed(2));
+// console.log(salary);
+
+// console.log(Number('please')); // NaN
+
+// console.log(5 / 0); //Infinity
+
+// Math
+
+// const result = Math.pow(2, 5);
+// console.log(result);
+
+// let number = prompt('Введите число');
+// number = Number(number);
+
+// let pow = prompt('Введите степень');
+// pow = Number(pow);
+
+// const result = number ** pow;
+
+// alert(result);
+
+// console.log(Math.random());
+
+// const max = 50;
+// const min = 20;
+
+// const result = Math.round(Math.random() * (max - min) + min);
+// console.log(result);
---
+++ js/script3-string.js
@@ -0,0 +1,30 @@
+const message = 'hello';
+
+console.log('Length is:', message.length);
+
+let jj = '5';
+let js = '7';
+
+console.log(Number(jj + js));
+
+const firstName = 'Chelsy';
+const lastName = 'Emerald';
+
+const room = 716;
+const type = 'VIP';
+
+// const welcomeMessage =
+//   'Гость' + ' ' +
+//   firstName +
+//   ' ' +
+//   lastName + ' ' +
+//   'поселяется в' + ' ' +
+//   type + ' ' +
+//   'номер:' + ' ' +
+//   room;
+// console.log(welcomeMessage);
+
+console.log(
+  `Гость ${lastName} ${firstName} поселяется в ${type} номер: ${room}.`
+);
+
