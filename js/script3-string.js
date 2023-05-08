1ca0df496e7c6a0346f055c689cd8e42c81dd7a6
Bogdan <ki4ack.bogdan@gmail.com>
2023-05-08 08:19:26 +0300

    gg

--- js/script3-string.js
+++ js/script3-string.js
@@ -7,24 +7,35 @@ let js = '7';
 
 console.log(Number(jj + js));
 
-const firstName = 'Chelsy';
-const lastName = 'Emerald';
+// const firstName = 'Chelsy';
+// const lastName = 'Emerald';
 
-const room = 716;
-const type = 'VIP';
+// const room = 716;
+// const type = 'VIP';
 
-// const welcomeMessage =
-//   'Гость' + ' ' +
-//   firstName +
-//   ' ' +
-//   lastName + ' ' +
-//   'поселяется в' + ' ' +
-//   type + ' ' +
-//   'номер:' + ' ' +
-//   room;
-// console.log(welcomeMessage);
+// // const welcomeMessage =
+// //   'Гость' + ' ' +
+// //   firstName +
+// //   ' ' +
+// //   lastName + ' ' +
+// //   'поселяется в' + ' ' +
+// //   type + ' ' +
+// //   'номер:' + ' ' +
+// //   room;
+// // console.log(welcomeMessage);
 
-console.log(
-  `Гость ${lastName} ${firstName} поселяется в ${type} номер: ${room}.`
-);
+// console.log(
+//   `Гость ${lastName} ${firstName} поселяется в ${type} номер: ${room}.`
+// );
+
+
+const quantity = 15;
+
+
+const brand = 'Samsung';
+const normalizedBrand = brand.toLowerCase();
+
+const orderMassage = `You order ${quantity} ${normalizedBrand} freeg`;
+
+console.log(orderMassage);
 
