43e3f1356aaa59b97d1399d60a2d9075645e3114
Bogdan <ki4ack.bogdan@gmail.com>
2023-05-09 20:54:57 +0300

    changes

---
+++ js/module-1/2_lesson/if_case.js
@@ -0,0 +1,91 @@
+const stars = 1;
+let price;
+
+// ----------------- IF ---------------------
+
+// if (stars === 1) {
+//   price = 10;
+// } else if (stars === 2) {
+//   price = 20;
+// } else if (stars === 3) {
+//   price = 30;
+// } else if (stars === 4) {
+//   price = 40;
+// } else if (stars === 5) {
+//   price = 120;
+// } else {
+//   console.log('Такого нету');
+// }
+
+// ----------------- CASE ---------------------
+
+// switch (stars) {
+//   case 1:
+//     price = 20;
+//     break;
+
+//   case 2:
+//     price = 30;
+//     break;
+
+//   case 3:
+//     price = 50;
+//     break;
+
+//   default:
+//     console.log('Такого нету');
+// }
+// console.log(price);
+
+// ----------------- IF OR ---------------------
+
+// if (stars === 1 || stars === 2) {
+//   price = 10;
+// } else if (stars === 3 || stars === 4) {
+//   price = 20;
+// } else if (stars === 5) {
+//   price = 30;
+// } else {
+//   console.log('Такого нету');
+// }
+
+// ----------------- CASE AND ---------------------
+
+// switch (stars) {
+//   case 1:
+//   case 2:
+//     price = 20;
+//     break;
+
+//   case 3:
+//   case 4:
+//     price = 50;
+//     break;
+
+//   case 5:
+//     price = 120;
+//     break;
+
+//   default:
+//     console.log('Такого нету');
+// }
+// console.log(price);
+
+// ----------------- ZAD 1 ---------------------
+
+let option = 5;
+let message;
+
+switch (option) {
+  case 1:
+    console.log('12:00');
+    break;
+  case 2:
+    console.log('9-12');
+    break;
+  case 3:
+    console.log('Today');
+    break;
+  default:
+    console.log('We will call you!');
+}
