bf12f36e9c0e744934d0169685ee80cd2c415836
BogdanWazer <ki4ack.bogdan@gmail.com>
2023-06-12 10:02:12 +0300

    up

--- js/module-3/1-lesson/03-cart.js
+++ js/module-3/1-lesson/03-cart.js
@@ -1,11 +1,11 @@
-const friends = [
-  { name: 'Mango', online: false },
-  { name: 'Kiwi', online: true },
-  { name: 'Poly', online: false },
-  { name: 'Ajax', online: true },
-  { name: 'Brandon', online: true },
-  { name: 'Jak', online: true },
-];
+// const friends = [
+//   { name: 'Mango', online: false },
+//   { name: 'Kiwi', online: true },
+//   { name: 'Poly', online: false },
+//   { name: 'Ajax', online: true },
+//   { name: 'Brandon', online: true },
+//   { name: 'Jak', online: true },
+// ];
 
 // for (const friend of friends) {
 //   console.log(friend.name); // get name value
@@ -29,23 +29,31 @@ const friends = [
 
 // console.log(findFriendByName(friends, 'Kiwi'));
 
-const findStatusByValue = function (storage, neededUserStatus) {
-  for (const userStatus of storage) {
-    if (userStatus.online === neededUserStatus) {
-      return 'Online!';
-    }
-  }
-};
+// const findStatusByValue = function (storage, neededUserStatus) {
+//   for (const userStatus of storage) {
+//     if (userStatus.online === neededUserStatus) {
+//       return 'Online!';
+//     }
+//   }
+// };
 
-console.log(findStatusByValue(friends, true));
+// console.log(findStatusByValue(friends, true));
 
-const getAllNames = function (allFriends) {
-  const names = [];
+// const getAllNames = function (allFriends) {
+//   const names = [];
 
-  for (const friend of allFriends) {
-    names.push(friend.name);
-  }
-  return names;
-};
+//   for (const friend of allFriends) {
+//     names.push(friend.name);
+//   }
+//   return names;
+// };
 
-console.log(getAllNames(friends));
+// console.log(getAllNames(friends));
+
+// const getOnlineFriends = function(onlineFriends) {
+//   for(const onlineFriend of onlineFriends) {
+//     console.log(onlineFriend);
+//   }
+// }
+
+// getOnlineFriends(friends);
