7496b62845abb4e89032c2edc01a459965573615
BogdanWazer <ki4ack.bogdan@gmail.com>
2023-06-20 21:02:46 +0300

    =

--- js/module-4/1-lesson/callback.js
+++ js/module-4/1-lesson/callback.js
@@ -29,7 +29,52 @@ doMath(2, 3, sub);
 const buttonRef = document.querySelector('.js-button');
 
 const handleBtnClick = function () {
-  console.log('Click');
+  console.log('Click' + Date.now());
 };
 
 buttonRef.addEventListener('click', handleBtnClick);
+
+function addEventListener(eventType, callback) {
+  if (eventType === event) {
+    callback();
+  }
+}
+
+const onGetPositionSuccess = function (position) {
+  console.log(position);
+};
+
+const onGetPositionError = function (error) {
+  console.log(error);
+};
+
+window.navigator.geolocation.getCurrentPosition(
+  onGetPositionError,
+  onGetPositionSuccess
+);
+
+const callback = function () {
+  console.log('Some');
+};
+setTimeout(callback, 2000);
+
+const onRequestSucces = function (response) {};
+
+const filter = function (array, test) {
+  const filteredArray = [];
+  for (const el of array) {
+    console.log(el);
+    console.log(test(el));
+  }
+
+  return filteredArray;
+};
+
+const callback1 = function (value) {
+  return value >= 3;
+};
+
+const r1 = filter([1, 2, 3, 4, 5, 6], callback1);
+console.log(r1);
+
+// const r2 = filter([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
