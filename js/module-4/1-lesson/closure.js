05067b58a0b0d64b6b8c45195991e63505745cc0
BogdanWazer <ki4ack.bogdam@gmail.com>
2023-06-23 23:14:11 +0300

    1

--- js/module-4/1-lesson/closure.js
+++ js/module-4/1-lesson/closure.js
@@ -2,7 +2,7 @@ const fnA = function (parametr) {
   const innerVariable = 'Fna variable';
 
   const innerFunction = function () {
-	console.log(parametr)
+    console.log(parametr);
     console.log(innerVariable);
     console.log('Inner');
   };
@@ -15,3 +15,15 @@ const fnB = fnA();
 fnB();
 
 console.log(fnB);
+
+const makeSheff = function (name) {
+  const makeDish = function (dish) {
+    console.log(`${name} coock ${dish}`);
+  };
+
+  return makeDish
+};
+
+const mango = makeSheff('Mango');
+
+mango('sss');
