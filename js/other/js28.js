d7572116e7a5e9dcf95f80506abf8e5beb8c1352
BogdanWazer <ki4ack.bogdam@gmail.com>
2023-07-15 21:40:50 +0300

    41234123

---
+++ js/other/js28.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const box = document.getElementById('box');
+console.log(box);
+
+const btns = document.getElementsByTagName('button');
+console.log(btns[0]);
+
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
+
+const hearts = document.querySelectorAll('.heart');
+console.log(hearts);
+
+const firstHeart = document.querySelector('.heart');
+console.log(firstHeart);
+
+const firstButton = document.querySelector('button');
+console.log(firstButton);
