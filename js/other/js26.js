baff275bce2ab63e97d3abb8787f751c20a03f83
BogdanWazer <ki4ack.bogdam@gmail.com>
2023-07-15 21:00:36 +0300

    2

---
+++ js/other/js26.js
@@ -0,0 +1,22 @@
+console.log('Hello world!');
+
+console.log(typeof (5 + ''));
+
+const num = 5;
+
+console.log('https://google.com/catalog/' + num);
+
+const fontSize = 26 + 'px';
+console.log(typeof Number(fontSize));
+console.log(typeof +'5');
+
+console.log(typeof parseInt('15px', 10)); // number
+
+// let answ = +prompt('Hello', '');
+// console.log(answ);
+
+let switcher = null;
+
+if (switcher) {
+  console.log('Working');
+}
