467bb62046b94e411d822c5268d9b68512a57952
BogdanWazer <ki4ack.bogdam@gmail.com>
2023-07-15 23:29:55 +0300

    1234134

---
+++ js/other/js29.js
@@ -0,0 +1,10 @@
+const box = document.getElementById('box'),
+  btns = document.getElementsByTagName('button'),
+  circles = document.getElementsByClassName('circle'),
+  hearts = document.querySelectorAll('.heart'),
+  firstHeart = document.querySelector('.heart'),
+  firstButton = document.querySelector('button');
+
+box.style.backgroundColor = 'gray';
+box.style.height = '150px';
+box.style.width = '50px';
