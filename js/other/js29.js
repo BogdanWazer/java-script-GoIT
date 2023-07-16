ca8a43fcd94a20baa6e6c649c316a2e4da2a8984
BogdanWazer <ki4ack.bogdam@gmail.com>
2023-07-16 15:00:36 +0300

    somne

--- js/other/js29.js
+++ js/other/js29.js
@@ -8,3 +8,22 @@ const box = document.getElementById('box'),
 box.style.backgroundColor = 'gray';
 box.style.height = '150px';
 box.style.width = '50px';
+const num = 300;
+box.style.cssText = `background-color: blue; width: 500px; height: ${num}px`;
+
+// for (let i = 0; i < hearts.length; i++) {
+//   hearts[i].style.backgroundColor = 'red';
+// }
+
+hearts.forEach((item) => {
+  item.style.backgroundColor = 'red';
+});
+
+const div = document.createElement('div');
+div.classList.add('black');
+
+div.style.cssText = 'weight: 100px; height: 100px; ';
+document.body.appendChild(div);
+div.innerHTML = '<h1>Hello!</h1>';
+
+div.insertAdjacentHTML('afterend', 'Hello2');
